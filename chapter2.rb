dc506ab028881e6385b4d10d191c852170bc5ac7
TKG1 <kouheideal@gmail.com>
2021-10-14 14:26:04 +0900

    Ruby Practice2

---
+++ chapter2.rb
@@ -0,0 +1,31 @@
+#1
+puts 2+3
+#2
+r = 2 / 2
+puts r * r *3.14
+#3
+puts "Ruby"
+#4
+puts "abc"+"def"
+#5
+x = "123"
+y = "456"
+puts x.to_i + y.to_i
+#6
+coffee = 300
+espresso = 100
+puts "コーヒー:#{coffee}円"
+puts "合計:#{coffee+espresso*2}円"
+#7
+coffee = 400
+espresso = 100
+puts "コーヒー:#{coffee}円"
+puts "合計:#{coffee+espresso*2}円"
+#8
+total = 300 * 2
+p total
+tax = total * 0.08
+puts tax
+#9
+total = 300 *2
+puts total
